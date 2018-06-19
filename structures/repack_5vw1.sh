f42515a1cdfa5e59848ec8edc22655545015470a
ckrivacic <krivacic@berkeley.edu>
2018-06-18 18:09:34 -0700

    Setup for repack/relax

---
+++ structures/repack_5vw1.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+#$ -S /bin/bash
+#$ -cwd
+#$ -o 1qjg_clean_relax_repack
+#$ -j y
+#$ -l mem_free=1G
+#$ -l arch=linux-x64
+#$ -l netapp=1G,scratch=1G
+#$ -l h_rt=12:00:00
+#$ -t 1-100
+
+source rosetta.sh
+repack_cas9 5vw1.pdb $SGE_TASK_ID
+
+
---
+++ structures/rosetta.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Expect $ROSETTA and $ROSETTA_BUILD to be set as environment variables.
+
+BIN=$ROSETTA/source/bin
+STRUCTS=$(pwd)
+LIGAND=$(readlink -f $STRUCTS/../ligand_2018)
+
+function header() {
+    echo "Host: $(hostname)"
+    echo "Date: $(date)"
+    echo "Directory: $(pwd)"
+    echo "Command: SGE_TASK_ID=$SGE_TASK_ID $0 $@"
+}
+
+function relax_cas9 () {
+    header
+    $BIN/relax.$ROSETTA_BUILD                                               \
+        -in:file:s $STRUCTS/$1                                              \
+        -out:suffix _relax/$2                                               \
+        -out:no_nstruct_label                                               \
+        -out:overwrite                                                      \
+        -relax:constrain_relax_to_start_coords                              \
+        -relax:coord_constrain_sidechains                                   \
+        -relax:ramp_constraints false                                       \
+        -packing:ex1                                                        \
+        -packing:ex2                                                        \
+        -packing:use_input_sc                                               \
+        -packing:flip_HNQ                                                   \
+        -packing:no_optH false                                              #
+}
+
+function repack_cas9 () {
+    header
+    $BIN/fixbb.$ROSETTA_BUILD                                               \
+        -in:file:s $STRUCTS/$1                                              \
+        -out:suffix _repack/$2                                              \
+        -out:no_nstruct_label                                               \
+        -out:overwrite                                                      \
+        -packing:resfile repack_and_e38                                     \
+        -packing:ex1                                                        \
+        -packing:ex2                                                        \
+        -packing:use_input_sc                                               #
+}
+
+function score_cas9_models () {
+    rm -f $1/score.tab
+    $BIN/score.$ROSETTA_BUILD                                               \
+        -in:file:s                                                          \
+            $STRUCTS/*.pdb                                                  \
+            $STRUCTS/$1/*.pdb                                               \
+        -in:file:native $STRUCTS/1qjg_clean.pdb                             \
+        -out:file:scorefile $1/score.tab                                    \
+        -out:file:scorefile_format json                                     #
+}
