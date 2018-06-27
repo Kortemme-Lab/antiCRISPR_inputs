1dc1facae9c3b430f4bb95a4e8fe14f97228ba0e
ckrivacic <krivacic@berkeley.edu>
2018-06-27 11:23:46 -0700

    relaxed input structure

---
+++ structures/qsub_relax_5vw1.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env sh
+
+rm -rf 5vw1_repack_relax
+mkdir -p 5vw1_repack_relax
+qsub relax_5vw1.sh
--- structures/relax_5vw1.sh
+++ structures/relax_5vw1.sh
@@ -3,15 +3,16 @@ set -euo pipefail
 
 #$ -S /bin/bash
 #$ -cwd
-#$ -o 1qjg_clean_relax
+#$ -o 5vw1_repack_relax
 #$ -j y
-#$ -l mem_free=1G
+#$ -l mem_free=4G
 #$ -l arch=linux-x64
 #$ -l netapp=1G,scratch=1G
-#$ -l h_rt=12:00:00
+#$ -l h_rt=24:00:00
 #$ -t 1-100
 
+hostname
 source rosetta.sh
-relax_cas9 5vw1_clean.pdb $SGE_TASK_ID
+relax_cas9 5vw1_repack.pdb $SGE_TASK_ID
 
 
